406f1bcd793f9cbbb6e6a09d357184a0245502f3
JakesterIX <mr.jake9@gmail.com>
2022-09-02 16:58:51 -0700

    Minigame Madness

--- games/hangman/hangman.js
+++ games/hangman/hangman.js
@@ -51,7 +51,7 @@ function handleGuess(chosenLetter){
 }
 
 function updatePic(){
-    hangPic.src = `./hangmanPics/${mistakes}.jpg`
+    hangPic.src = `./hangmanPics/${mistakes}.JPG`
 }
 
 function checkGameWon(){
