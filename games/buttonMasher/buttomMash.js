e3690025d180987c43f19f11adda37e3fbe0a51e
JakesterIX <mr.jake9@gmail.com>
2022-09-02 16:19:32 -0700

    Minigame Website

---
+++ games/buttonMasher/buttomMash.js
@@ -0,0 +1,42 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const start = document.querySelector('.mash-start');
+
+let score = 0;
+
+start.addEventListener('click', () => {
+    score = 0;
+    p.innerText = "";
+    countdown();
+      setTimeout(() => {
+      aKeyPress();
+      setTimeout(() => {
+        h1.innerText = `You mashed ${score} times!`
+        p.innerText = "click start to try for a better score!"
+      }, 5000)
+    }, 1500)
+})
+
+function countdown() {
+        setTimeout(() => {
+            setTimeout(() => {
+                setTimeout(() => {
+                    h1.innerText = "GO GO GO!!"
+                },500)
+                h1.innerText = "1"
+            },500)
+            h1.innerText = "2"
+        },500)
+        h1.innerText = "3"
+}
+
+function aKeyPress() {
+    document.body.onkeyup = function(e) {
+        if (e.key == " " ||
+            e.code == "KeyA"  
+        ) {
+          score += 1;
+        }
+      }
+}
+
