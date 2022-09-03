79665fda1b74be4a45c43637ab6d44ec2b096d60
JakesterIX <mr.jake9@gmail.com>
2022-09-02 18:16:10 -0700

    Minigame Madness

--- games/buttonMasher/buttomMash.js
+++ games/buttonMasher/buttomMash.js
@@ -14,21 +14,23 @@ start.addEventListener('click', () => {
       setTimeout(() => {
         h1.innerText = `You mashed ${score} times!`
         p.innerText = "click start to try for a better score!"
+        start.id = ""
       }, 5000)
     }, 1500)
 })
 
 function countdown() {
+  start.id = "mash-hide";
         setTimeout(() => {
             setTimeout(() => {
                 setTimeout(() => {
-                    h1.innerText = "GO GO GO!!"
+                    h1.innerText = "GO GO GO!!";
                 },500)
-                h1.innerText = "1"
+                h1.innerText = "1";
             },500)
-            h1.innerText = "2"
+            h1.innerText = "2";
         },500)
-        h1.innerText = "3"
+        h1.innerText = "3";
 }
 
 function addScore() {
